71f7986251bfde610bb4cf66f487310415c01eee
alonecheer <alonecheer@hotmail.com>
2021-01-18 00:48:13 +0700

    :package: setup libary

---
+++ pages/styles.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+const StyleWrapper = styled.div`
+  *,
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "Prompt", sans-serif;
+  }
+  :root {
+    --primary: #22d2a0;
+    --secondary: #192824;
+    --background: #192824;
+    --green: #1fc11b;
+    --yellow: #ffd913;
+    --orange: #ff9c55;
+    --red: #ff5555;
+    --gray: #7f7f7f;
+    --reddg: #e2071b;
+    --buledg: #212554;
+  }
+`;
+export default StyleWrapper;
